9fce2619e9ef95b9f7c52d45c422ab24da743f94
ms161 <msehrawat161@gmail.com>
2024-01-31 01:26:07 +0530

    task 9

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,23 +1,55 @@
-const Message=require('../models/messageModel')
-function Invalidstring(str){
-    if(str.trim().length==0 || str == undefined){
+const Message = require('../models/messageModel')
+const User = require('../models/userModel')
+function Invalidstring(str) {
+    if (str.trim().length == 0 || str == undefined) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
-exports.message = async (req, res) => {
-try{
-    const { message } = req.body
-    console.log(message)
-    if(Invalidstring(message)){
-        return res.status(400).json({message:'Type some message to send'})
+
+
+exports.postMessage = async (req, res) => {
+    try {
+        const { message } = req.body
+        console.log(message)
+        if (Invalidstring(message)) {
+            return res.status(400).json({ message: 'Type some message to send' })
+        }
+        const data = await Message.create({
+            message: message,
+            userId: req.user.id
+        })
+        res.status(200).json({ newMessage: data });
     }
-    const data=await Message.create({
-        message:message,
-        userId:req.user.id 
-    })
-    res.status(200).json({newMessage: data});}
-    catch(err){console.log(err)}
+    catch (err) {
+        console.log(err)
+        res.status(500).json({ message: err })
+    }
+}
+
+exports.getMessages = async (req, res) => {
+    try {
+        const allMessages = await Message.findAll({
+            attributes: ['id', 'message'],
+
+            include: [{
+                model: User,
+                attributes: ['name'],
+                required: true
+            }],
+
+        })
+        console.log(allMessages)
+        allMessages.forEach(element => {
+            console.log(element.message, '>>>>>>>>>>>>>')
+        });
+        res.status(200).json({ messages: allMessages })
+
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({ message: err })
+    }
 }
--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -4,6 +4,8 @@ const router=express.Router()
 const messageController=require('../controllers/messageController')
 const auth=require('../middlewares/authentication')
 
-router.post('/message',auth.authenticate,messageController.message)
+router.post('/postMessage',auth.authenticate,messageController.postMessage)
+
+router.get('/getMessages',auth.authenticate,messageController.getMessages)
 
 module.exports=router
